32a28f54b7e1f8be11dbe46bbda2b3ae1f97eaf9
hilina88 <hilinawuhib@gmail.com>
2022-02-11 12:32:08 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,15 +2,15 @@ import express from "express";
 import productsRoutes from "./services/products/product.js";
 import reviewsRoutes from "./services/reviews/review.js";
 import usersRoutes from "./services/reviews/user.js";
-import categorysRoutes from "./services/products/category.js"
+import categorysRoutes from "./services/products/category.js";
 import { authenticateDatabase } from "./utils/db/connect.js";
 const server = express();
 const { PORT = 3006 } = process.env;
 server.use(express.json());
 server.use("/products", productsRoutes);
 server.use("/reviews", reviewsRoutes);
-server.use("/users", usersRoutes)
-server.use("/categories",categorysRoutes)
+server.use("/users", usersRoutes);
+server.use("/categories", categorysRoutes);
 server.listen(PORT, () => {
   authenticateDatabase();
   console.log(`Server is listening on port ${PORT}`);
