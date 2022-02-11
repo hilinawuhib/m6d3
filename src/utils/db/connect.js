2b0b59ede0ba501fddea8e7cf7b56dcfd5c2cd4f
hilina88 <hilinawuhib@gmail.com>
2022-02-11 12:45:02 +0100

    Update connect.js

--- src/utils/db/connect.js
+++ src/utils/db/connect.js
@@ -1,19 +1,20 @@
 import Sequelize from "sequelize";
-const { DB_LINK } = process.env;
+
+const {DB_LINK} = process.env;
 const sequelize = new Sequelize(process.env.DB_LINK, {
   dialect: "postgres",
   protocol: "postgres",
   dialectOptions: {
     ssl: {
-      require: true,
-      rejectUnauthorized: false,
+      require:true,
+       rejectUnauthorized: false,
     },
   },
 });
 export const authenticateDatabase = async () => {
   try {
     await sequelize.authenticate({ logging: false });
-    await sequelize.sync({ alter: true, logging: false });
+    await sequelize.sync({ force: true, logging: false });
     console.log("connection had been established");
   } catch (error) {
     console.log(error);
